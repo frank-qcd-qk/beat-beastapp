1b8cb926dbd55af1eb4f740a4eb7534a6658b533
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-06-23 11:34:04 -0400

    fix webformat

--- master/main.py
+++ master/main.py
@@ -23,45 +23,46 @@ mW = 640
 mH = 480
 # start looping over all the frames
 while True:
-	# receive RPi name and frame from the RPi and acknowledge
-	# the receipt
-	(rpiName, frame) = imageHub.recv_image()
-	imageHub.send_reply(b'OK')
-	# if a device is not in the last active dictionary then it means
-	# that its a newly connected device
-	if rpiName not in lastActive.keys():
-		print("[INFO] receiving data from {}...".format(rpiName))
-	# record the last active time for the device from which we just
-	# received a frame
-	lastActive[rpiName] = datetime.now()
+    print("heartbeat!")
+     # receive RPi name and frame from the RPi and acknowledge
+     # the receipt
+    (rpiName, frame) = imageHub.recv_image()
+    imageHub.send_reply(b'OK')
+       # if a device is not in the last active dictionary then it means
+       # that its a newly connected device
+    if rpiName not in lastActive.keys():
+        print("[INFO] receiving data from {}...".format(rpiName))
+        # record the last active time for the device from which we just
+        # received a frame
+    lastActive[rpiName] = datetime.now()
     # resize the frame to have a maximum width of 400 pixels, then
-	# grab the frame dimensions and construct a blob
-	frame = imutils.resize(frame, width=400)
-	(h, w) = frame.shape[:2]
-	# update the new frame in the frame dictionary
-	frameDict[rpiName] = frame
-	# build a montage using images in the frame dictionary
-	montages = build_montages(frameDict.values(), (w, h), (mW, mH))
-	# display the montage(s) on the screen
-	for (i, montage) in enumerate(montages):
-		cv2.imshow("Beat the Beast App ({})".format(i),
-			montage)
-	# detect any kepresses
-	key = cv2.waitKey(1) & 0xFF
+        # grab the frame dimensions and construct a blob
+    frame = imutils.resize(frame, width=400)
+    (h, w) = frame.shape[:2]
+    # update the new frame in the frame dictionary
+    frameDict[rpiName] = frame
+    # build a montage using images in the frame dictionary
+    montages = build_montages(frameDict.values(), (w, h), (mW, mH))
+    # display the montage(s) on the screen
+    for (i, montage) in enumerate(montages):
+        cv2.imshow("Beat the Beast App ({})".format(i),
+                   montage)
+        # detect any kepresses
+    key = cv2.waitKey(1) & 0xFF
     # was made is greater than the threshold set then do a check
-	if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
-		# loop over all previously active devices
-		for (rpiName, ts) in list(lastActive.items()):
-			# remove the RPi from the last active and frame
-			# dictionaries if the device hasn't been active recently
-			if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
-				print("[INFO] lost connection to {}".format(rpiName))
-				lastActive.pop(rpiName)
-				frameDict.pop(rpiName)
-		# set the last active check time as current time
-		lastActiveCheck = datetime.now()
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
+            # loop over all previously active devices
+            for (rpiName, ts) in list(lastActive.items()):
+                # remove the RPi from the last active and frame
+                # dictionaries if the device hasn't been active recently
+                if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
+                    print("[INFO] lost connection to {}".format(rpiName))
+                    lastActive.pop(rpiName)
+                    frameDict.pop(rpiName)
+            # set the last active check time as current time
+            lastActiveCheck = datetime.now()
+        # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
 # do a bit of cleanup
 cv2.destroyAllWindows()
