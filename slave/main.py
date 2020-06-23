9113f3b576a3655b0c306abaea8c7c38de16ea8d
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-06-23 12:42:12 -0400

    fix tab

--- slave/main.py
+++ slave/main.py
@@ -5,7 +5,7 @@ import socket
 import time
 
 #! Master IP: 10.0.0.1
-#! Slave use port: 555{ID} 
+#! Slave use port: 555{ID}
 DEBUG = True
 
 
@@ -14,19 +14,19 @@ sender = imagezmq.ImageSender(connect_to="tcp://10.0.0.1:5555")
 rpiName = socket.gethostname()
 
 if DEBUG:
-	print("Got Hostname: ", rpiName)
+    print("Got Hostname: ", rpiName)
 
 vs = VideoStream(usePiCamera=True).start()
 #stream = VideoStream(src=0).start
 time.sleep(2.0)
 
 while True:
-	if DEBUG:
-		print("Sending....")
-	frame =vs.read()
-	if frame is not None:
-		sender.send_image(rpiName, frame)
-	else:
+    if DEBUG:
+        print("Sending....")
+    frame = vs.read()
+    if frame is not None:
+        sender.send_image(rpiName, frame)
+    else:
     	print("No frame to send {}".format(time.time()))
-	if DEBUG:
-		print("Sent!")
+    if DEBUG:
+        print("Sent!")
